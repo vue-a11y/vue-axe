b01015e13bdd70f2e6bb4211f0f86aceaebc9caf
Alan Albuquerque (ktquez) <ktquez@gmail.com>
2018-05-15 20:41:23 -0300

    Install and config Cypress to Automated Acceptance Testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
   root: true,
   env: {
+    "cypress/globals": true,
     browser: true,
     node: true
   },
   extends: 'standard',
+  plugins: [
+    "cypress"
+  ],
   // add your custom rules here
   rules: {},
   globals: {}
