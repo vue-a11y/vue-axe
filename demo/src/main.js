a5e5e4812a88f75add790619736b7f0636ddf9cc
Alan Albuquerque (ktquez) <ktquez@gmail.com>
2018-05-15 20:43:08 -0300

    Install plugin only in development environment

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,21 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vueAxe from '../../vue-axe'
+import VueAxe from '../../vue-axe'
 
-Vue.use(vueAxe, {
-  config: {
-    rules: [
-      { id: 'heading-order', enabled: true },
-      { id: 'label-title-only', enabled: true },
-      { id: 'link-in-text-block', enabled: true },
-      { id: 'region', enabled: true },
-      { id: 'skip-link', enabled: true },
-      { id: 'help-same-as-label', enabled: true }
-    ]
-  }
-})
-
-Vue.config.productionTip = false
+if (process.env.NODE_ENV !== 'production') {
+  Vue.use(VueAxe, {
+    config: {
+      rules: [
+        { id: 'heading-order', enabled: true },
+        { id: 'label-title-only', enabled: true },
+        { id: 'link-in-text-block', enabled: true },
+        { id: 'region', enabled: true },
+        { id: 'skip-link', enabled: true },
+        { id: 'help-same-as-label', enabled: true }
+      ]
+    }
+  })
+  Vue.config.productionTip = false
+}
 
 /* eslint-disable no-new */
 new Vue({
