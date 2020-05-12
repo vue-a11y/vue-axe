69a2d9b7ac9599e18595916959f0980726aef25d
Alan Ktquez <ktquez@gmail.com>
2020-05-12 12:41:20 -0300

    Update demo

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,20 +1,11 @@
 import Vue from 'vue'
+import VueAxe from '../vue-axe'
 import App from './App.vue'
 import router from './router.js'
 
 // Use this plugin only in development => if (process.env.NODE_ENV !== 'production')
-const VueAxe = require('../../dist/vue-axe')
 Vue.use(VueAxe, {
-  config: {
-    rules: [
-      { id: 'heading-order', enabled: true },
-      { id: 'label-title-only', enabled: true },
-      { id: 'link-in-text-block', enabled: true },
-      { id: 'region', enabled: true },
-      { id: 'skip-link', enabled: true },
-      { id: 'help-same-as-label', enabled: true }
-    ]
-  }
+  clearConsoleOnUpdate: true
 })
 Vue.config.productionTip = false
 
